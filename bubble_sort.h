ab5361f3ffef346fb5c8204f89e309678d72d8db
Stefanos Savvidis <stefano@savvidis.ca>
2018-03-26 13:17:31 -0400

    0.1

---
+++ bubble_sort.h
@@ -0,0 +1,31 @@
+
+
+/* 
+File name is bubble_sort.h
+ Purpose: file contains a prototype for a bubble sort function
+ 
+ Revisions
+ Doron Nussbaum 2018
+
+
+ 
+ Copyright 2018 Doron Nussbaum
+
+ 
+ */
+
+
+#ifndef BUBBLE_SORT_2401_HEADER
+#define BUBBLE_SORT_2401_HEADER 1
+/************************************************************************/
+// INCLUDE FILES
+
+#include "linked_list.h"
+
+/************************************************************************/
+// FUNCTION PROTOTYPES
+
+void bubbleSort(PersonalInfo **head);
+
+
+#endif
